118738005ad24deb3a1d1a471a283ab587621761
mollycann <mollycann4567@gmail.com>
2018-08-10 18:32:47 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,18 @@
-# dog.rb
+class Dog 
+  def initalize(name)
+    @name= name  
+  end 
+  
+def name 
+  @name 
+end 
+
+def name= (new_name)
+  @color= new_name
+end 
+
+dog_one= Dog.new('silver')
+  puts dog_one.name 
+  dog_one.name= 'Lassie' 
+  puts dog_one.name 
+end 
